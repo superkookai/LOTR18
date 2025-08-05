0bb124699b422ec5b85fe801ca3bde0acd29a033
superkookai <weerawut@live.com>
2025-08-06 06:51:13 +0700

    Chapter 8

--- LOTR18/ContentView.swift
+++ LOTR18/ContentView.swift
@@ -8,14 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var leftAmount: String = ""
+    @State private var rightAmount: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150)
+                
+                Text("Currency Exchange")
+                    .font(.system(size: 40))
+                    .foregroundStyle(.white)
+                
+                HStack {
+                    //Left Conversion
+                    VStack {
+                        //Currency
+                        HStack {
+                            Image(.copperpenny)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 35)
+                            
+                            Text("Copper Penny")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        }
+                        
+                        //TextField
+                        TextField("Amount", text: $leftAmount)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    
+                    //Equal Sign
+                    Image(systemName: "equal")
+                        .font(.system(size: 40))
+                        .foregroundStyle(.white)
+                    
+                    
+                    //Right Conversion
+                    VStack {
+                        //Currency
+                        HStack {
+                            Text("Gold Penny")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                            Image(.goldpenny)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 35)
+                            
+                        }
+                        
+                        //TextField
+                        TextField("Amount", text: $rightAmount)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                }
+                .padding()
+                .background(Color.black.opacity(0.3))
+                
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
