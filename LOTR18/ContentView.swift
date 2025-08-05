0c085daea06f1163acf5de50d4766267ad90c25d
superkookai <weerawut@live.com>
2025-08-06 05:45:29 +0700

    Initial Commit

---
+++ LOTR18/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LOTR18
+//
+//  Created by Weerawut Chaiyasomboon on 06/08/2568.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTR18/LOTR18App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR18App.swift
+//  LOTR18
+//
+//  Created by Weerawut Chaiyasomboon on 06/08/2568.
+//
+
+import SwiftUI
+
+@main
+struct LOTR18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
